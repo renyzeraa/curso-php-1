1f4f911b2fe95300e4f1dc5d63b320de4e7923cc
Renan Silva <renansilvaytB@gmail.com>
2023-01-23 20:50:57 -0300

    01-07-superglobais

---
+++ modulo01/conteudo/superglobais.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<pre>';
+//var_dump($_SERVER);
+//var_dump($_GET); // dados enviados via get
+//var_dump($_POST);  // dados enviados via post
+var_dump($_REQUEST);  // aquilo que envia via get e via post
+echo '<pre>';
+
+?>
